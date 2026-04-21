8684641ad6d4fde44f6a81e33f9f4479e147fef8
Younes <Younes>
2026-04-21 16:12:47 +0200

    failed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function calculateWeather(temp) {
-  if (temp > 25) return "Hot";
-  if (temp < 10) return "Cold";
+  if (temp > 25) return "Cold";
+  if (temp < 10) return "Hot";
   return "Mild";
 }
 module.exports = calculateWeather;
