673331de3cf40084cbb8a6480d15ee583f0caeef
Younes <Younes>
2026-04-21 16:16:45 +0200

    true

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function calculateWeather(temp) {
-  if (temp > 25) return "Cold";
-  if (temp < 10) return "Hot";
+  if (temp > 25) return "Hot";
+  if (temp < 10) return "Cold";
   return "Mild";
 }
 module.exports = calculateWeather;
